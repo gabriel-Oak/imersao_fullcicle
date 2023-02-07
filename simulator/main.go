cbb351371699fd0432565c83d0e3d37155796178
gabriel-Oak <gabrielcarvalhocosta@live.com>
2023-02-07 15:17:11 -0300

    aula 1

---
+++ simulator/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	kafka2 "github.com/gabriel-Oak/imersaofsfc-simulator/application/kafka"
+	"github.com/gabriel-Oak/imersaofsfc-simulator/infra/kafka"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .ev file")
+	}
+}
+
+func main() {
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
+	}
+}
